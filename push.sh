358cd23dc86be4433aa8875831bbfb4476c50509
Jimmy Lam <dev.jimmylam@gmail.com>
2021-03-09 14:40:18 -0800

    updated project section

---
+++ push.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git add . --all
+
+# NOTE: Your commit message must be written in the following format "{{ your initial } - {{ your commit message goes here... }}"
+git commit -m "updated project section "
+
+git status
+
+git push
