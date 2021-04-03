8c42360f27ff39111bebba6c6d10a0005cc8e9ae
Jimmy Lam <dev.jimmylam@gmail.com>
2021-04-03 12:45:08 -0700

    updated CONTACT

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 git add . --all
 
 # NOTE: Your commit message must be written in the following format "{{ your initial } - {{ your commit message goes here... }}"
-git commit -m "updated project section"
+git commit -m "updated CONTACT"
 
 git status
 
