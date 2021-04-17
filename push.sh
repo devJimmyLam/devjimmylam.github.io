3a61fa9df2d2c758087538668df9687e5d065690
Jimmy Lam <dev.jimmylam@gmail.com>
2021-04-17 15:02:24 -0700

    deployed flowerseverafter project

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 git add . --all
 
 # NOTE: Your commit message must be written in the following format "{{ your initial } - {{ your commit message goes here... }}"
-git commit -m "updated resume"
+git commit -m "deployed flowerseverafter project"
 
 git status
 
