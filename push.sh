e56a291fb9cb89df6dd5c9f6b3f5d1fe2525e217
Jimmy Lam <dev.jimmylam@gmail.com>
2021-05-12 12:44:50 -0700

    added api documentation for project

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 git add . --all
 
 # NOTE: Your commit message must be written in the following format "{{ your initial } - {{ your commit message goes here... }}"
-git commit -m "updated resume"
+git commit -m "added api documentation for project"
 
 git status
 
